d3cfc1710431525c2cb1dc2b20490d6c479fa5fa
Lupo511 <alberto.scalzini@alice.it>
2016-11-23 16:48:57 +0100

    More debug

--- ManagedAssembly/MultipleBombsAssembly/MultipleBombsAssembly/MultipleBombs.cs
+++ ManagedAssembly/MultipleBombsAssembly/MultipleBombsAssembly/MultipleBombs.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using UnityEngine;
 
 namespace MultipleBombsAssembly
 {
-    public class MultipleBombs : KMService
+    public class MultipleBombs : MonoBehaviour
     {
         private Rect windowRect;
 
@@ -32,6 +33,7 @@ namespace MultipleBombsAssembly
                 GUILayout.BeginVertical();
                 if (GUILayout.Button("Generate new bomb"))
                 {
+                    Debug.Log("[MultipleBombs]New bomb requested");
                     GameplayState gameplayState = FindObjectOfType<GameplayState>();
                     BombGenerator bombGenerator = FindObjectOfType<BombGenerator>();
                     if (gameplayState != null)
@@ -39,8 +41,33 @@ namespace MultipleBombsAssembly
                         if (bombGenerator != null)
                         {
                             Debug.Log("[MultipleBombs]Generating new bomb");
-                            bombGenerator.CreateBomb(gameplayState.Mission.GeneratorSetting, gameplayState.Room.BombSpawnPosition, (new System.Random()).Next(), Assets.Scripts.Missions.BombTypeEnum.Default);
+
+                            gameplayState.Room.BombSpawnPosition.transform.position += new Vector3(0.5f, 0, 0);
+
+                            Bomb bomb = bombGenerator.CreateBomb(gameplayState.Mission.GeneratorSetting, gameplayState.Room.BombSpawnPosition, (new System.Random()).Next(), Assets.Scripts.Missions.BombTypeEnum.Default);
+
+                            gameplayState.Room.BombSpawnPosition.transform.position -= new Vector3(0.5f, 0, 0);
+
+                            GameObject roomGO = (GameObject)gameplayState.GetType().GetField("roomGO", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(gameplayState);
+
+                            Selectable mainSelectable = roomGO.GetComponent<Selectable>();
+                            List<Selectable> childern = mainSelectable.Children.ToList();
+                            childern.Insert(2, bomb.GetComponent<Selectable>());
+                            mainSelectable.Children = childern.ToArray();
+                            bomb.GetComponent<Selectable>().Parent = mainSelectable;
+                            bomb.GetTimer().gameObject.SetActive(false);
+                            bomb.GetTimer().LightGlow.enabled = false;
+
                             Debug.Log("[MultipleBombs]Bomb generated");
+
+                            bomb.WidgetManager.ActivateAllWidgets();
+                            if (!bomb.HasDetonated)
+                            {
+                                foreach (BombComponent component in bomb.BombComponents)
+                                {
+                                    component.Activate();
+                                }
+                            }
                         }
                     }
                 }
