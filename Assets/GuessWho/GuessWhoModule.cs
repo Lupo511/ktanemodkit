22ed1c0032a7d26bc50e4c1ebfb3af45c9e544c6
Lupo511 <alberto.scalzini@alice.it>
2017-03-31 19:32:59 +0200

    Added RenderTexture

---
+++ Assets/GuessWho/GuessWhoModule.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuessWhoModule : MonoBehaviour
+{
+    public Texture2D ComponentBaseTexture;
+    public RenderTexture ModuleRenderTexture;
+
+    public void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void OnGUI()
+    {
+        if (Event.current.type == EventType.Repaint)
+        {
+            RenderTexture oldRenderTexture = RenderTexture.active;
+            RenderTexture.active = ModuleRenderTexture;
+            GL.PushMatrix();
+            GL.LoadPixelMatrix(0, 1024, 1024, 0);
+            GL.Clear(true, true, Color.white);
+            Graphics.DrawTexture(new Rect(0, 0, 1024, 1024), ComponentBaseTexture);
+            GL.PopMatrix();
+            RenderTexture.active = oldRenderTexture;
+        }
+    }
+}
