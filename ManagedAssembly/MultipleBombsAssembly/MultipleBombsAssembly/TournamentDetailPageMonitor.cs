525b23ccf0eb1949b72313e404c3e1760884ecf0
Lupo511 <alberto.scalzini@alice.it>
2018-07-13 14:54:20 +0200

    TournamentDetailPage now also checks every generator setting even if they're not used

--- ManagedAssembly/MultipleBombsAssembly/MultipleBombsAssembly/TournamentDetailPageMonitor.cs
+++ ManagedAssembly/MultipleBombsAssembly/MultipleBombsAssembly/TournamentDetailPageMonitor.cs
@@ -50,84 +50,85 @@ namespace MultipleBombsAssembly
 
             Mission currentMission = (Mission)page.GetType().GetField("currentMission", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(page);
             MultipleBombsMissionDetails missionDetails = MultipleBombsMissionDetails.ReadMission(currentMission);
-            if (missionDetails.BombCount > 1)
+            List<string> missingModTypes = new List<string>();
+            int maxModuleCount = Math.Max(11, ModManager.Instance.GetMaximumModules());
+            int maxFrontFaceModuleCount = Math.Max(5, ModManager.Instance.GetMaximumModulesFrontFace());
+            int requiredModuleCount = 0;
+            int requiredFrontFaceModuleCount = 0;
+            foreach (GeneratorSetting generatorSetting in missionDetails.GeneratorSettings.Values)
             {
-                List<string> missingModTypes = new List<string>();
-                int maxModuleCount = Math.Max(11, ModManager.Instance.GetMaximumModules());
-                int maxFrontFaceModuleCount = Math.Max(5, ModManager.Instance.GetMaximumModulesFrontFace());
-                int requiredModuleCount = 0;
-                int requiredFrontFaceModuleCount = 0;
-                foreach (GeneratorSetting generatorSetting in missionDetails.GeneratorSettings.Values)
+                if (generatorSetting.ComponentPools != null)
                 {
-                    if (generatorSetting.ComponentPools != null)
+                    foreach (ComponentPool pool in generatorSetting.ComponentPools)
                     {
-                        foreach (ComponentPool pool in generatorSetting.ComponentPools)
+                        foreach (string modType in pool.ModTypes)
                         {
-                            foreach (string modType in pool.ModTypes)
+                            if (!ModManager.Instance.HasBombComponent(modType))
                             {
-                                if (!ModManager.Instance.HasBombComponent(modType))
-                                {
-                                    missingModTypes.Add(modType);
-                                }
+                                missingModTypes.Add(modType);
                             }
                         }
                     }
-                    int moduleCount = generatorSetting.GetComponentCount();
-                    if (generatorSetting.FrontFaceOnly)
-                        requiredFrontFaceModuleCount = Math.Max(requiredFrontFaceModuleCount, moduleCount);
-                    else
-                        requiredModuleCount = Math.Max(requiredModuleCount, moduleCount);
                 }
-                if (missingModTypes.Count > 0)
-                {
-                    canStartField.SetValue(page, false);
-                    page.TextDescription.text = "Missing mods:\n" + string.Join("\n", missingModTypes.ToArray());
-                }
-                else if (requiredModuleCount > maxModuleCount)
-                {
-                    canStartField.SetValue(page, false);
-                    page.TextDescription.text = "A bomb that can support more modules is required.\n\nCurrent bombs only support up to " + maxModuleCount + " modules.";
-                }
-                else if (requiredFrontFaceModuleCount > maxFrontFaceModuleCount)
-                {
-                    canStartField.SetValue(page, false);
-                    page.TextDescription.text = "A bomb that can support more modules is required.\n\nCurrent bombs only support up to " + maxFrontFaceModuleCount + " modules.";
-                }
-                else if (missionDetails.BombCount > MultipleBombs.GetCurrentMaximumBombCount())
-                {
-                    canStartField.SetValue(page, false);
-                    page.TextDescription.text = "A room that can support more bombs is required.\n\nCurrent rooms only support up to " + MultipleBombs.GetCurrentMaximumBombCount() + " bombs.";
-                }
+                int moduleCount = generatorSetting.GetComponentCount();
+                if (generatorSetting.FrontFaceOnly)
+                    requiredFrontFaceModuleCount = Math.Max(requiredFrontFaceModuleCount, moduleCount);
                 else
-                {
-                    canStartField.SetValue(page, true);
-                    page.TextDescription.text = currentMission.Description;
-                }
+                    requiredModuleCount = Math.Max(requiredModuleCount, moduleCount);
+            }
+            if (missingModTypes.Count > 0)
+            {
+                canStartField.SetValue(page, false);
+                page.TextDescription.text = "Missing mods:\n" + string.Join("\n", missingModTypes.ToArray());
+            }
+            else if (requiredModuleCount > maxModuleCount)
+            {
+                canStartField.SetValue(page, false);
+                page.TextDescription.text = "A bomb that can support more modules is required.\n\nCurrent bombs only support up to " + maxModuleCount + " modules.";
+            }
+            else if (requiredFrontFaceModuleCount > maxFrontFaceModuleCount)
+            {
+                canStartField.SetValue(page, false);
+                page.TextDescription.text = "A bomb that can support more modules is required.\n\nCurrent bombs only support up to " + maxFrontFaceModuleCount + " modules.";
+            }
+            else if (missionDetails.BombCount > MultipleBombs.GetCurrentMaximumBombCount())
+            {
+                canStartField.SetValue(page, false);
+                page.TextDescription.text = "A room that can support more bombs is required.\n\nCurrent rooms only support up to " + MultipleBombs.GetCurrentMaximumBombCount() + " bombs.";
+            }
+            else
+            {
+                canStartField.SetValue(page, true);
+                page.TextDescription.text = currentMission.Description;
+            }
 
-                float time = missionDetails.GeneratorSettings[0].TimeLimit;
-                int modules = missionDetails.GeneratorSettings[0].GetComponentCount();
-                int strikes = missionDetails.GeneratorSettings[0].NumStrikes;
-                for (int i = 1; i < missionDetails.BombCount; i++)
+            if (missionDetails.BombCount > 1)
+            {
+                float maxTime = 0;
+                int totalModules = 0;
+                int totalStrikes = 0;
+                for (int i = 0; i < missionDetails.BombCount; i++)
                 {
                     GeneratorSetting generatorSetting;
                     if (missionDetails.GeneratorSettings.TryGetValue(i, out generatorSetting))
                     {
-                        if (generatorSetting.TimeLimit > time)
-                            time = generatorSetting.TimeLimit;
-                        modules += generatorSetting.GetComponentCount();
-                        strikes += generatorSetting.NumStrikes;
+                        if (generatorSetting.TimeLimit > maxTime)
+                            maxTime = generatorSetting.TimeLimit;
+                        totalModules += generatorSetting.GetComponentCount();
+                        totalStrikes += generatorSetting.NumStrikes;
                     }
                     else
                     {
-                        modules += missionDetails.GeneratorSettings[0].GetComponentCount();
-                        strikes += missionDetails.GeneratorSettings[0].NumStrikes;
+                        totalModules += missionDetails.GeneratorSettings[0].GetComponentCount();
+                        totalStrikes += missionDetails.GeneratorSettings[0].NumStrikes;
                     }
                 }
-                page.TextTime.text = string.Format("{0}:{1:00}", (int)time / 60, time % 60);
-                page.TextModuleCount.text = modules + (modules == 1 ? " Module" : " Modules");
+
+                page.TextTime.text = string.Format("{0}:{1:00}", (int)maxTime / 60, maxTime % 60);
+                page.TextModuleCount.text = totalModules + (totalModules == 1 ? " Module" : " Modules");
                 page.TextStrikes.alignment = TMPro.TextAlignmentOptions.Right;
                 page.TextStrikes.enableAutoSizing = false;
-                page.TextStrikes.text = missionDetails.BombCount + " Bombs\n" + strikes + (strikes == 1 ? " Strike" : " Strikes") + "\n ";
+                page.TextStrikes.text = missionDetails.BombCount + " Bombs\n" + totalStrikes + (totalStrikes == 1 ? " Strike" : " Strikes") + "\n ";
             }
         }
     }
